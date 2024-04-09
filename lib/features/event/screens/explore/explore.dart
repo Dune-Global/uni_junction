d2ed9dd1d59a143f48e3f9b2e562a948d08b9e9b
yasithranusha <yasithranusha24@gmail.com>
2024-04-10 00:35:31 +0530

    refactor: Category gride into widgets

--- lib/features/event/screens/explore/explore.dart
+++ lib/features/event/screens/explore/explore.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:uni_junction/common/widgets/appbar/appbar.dart';
-import 'package:uni_junction/features/event/screens/explore/widgets/catergory_card.dart';
+import 'package:uni_junction/features/event/screens/explore/widgets/categories_grid.dart';
 import 'package:uni_junction/features/event/screens/home/widgets/search_container.dart';
 import 'package:uni_junction/utils/constants/colors.dart';
 import 'package:uni_junction/utils/constants/sizes.dart';
-import 'package:uni_junction/data/services/explore/categories_data.dart';
 
 class ExploreScreen extends StatelessWidget {
   @override
@@ -48,21 +47,7 @@ class ExploreScreen extends StatelessWidget {
                   children: [
                     Container(
                       height: MediaQuery.of(context).size.height * 0.7,
-                      child: GridView.builder(
-                        itemCount: categories.length,
-                        gridDelegate:
-                            const SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 2,
-                          crossAxisSpacing: 10.0,
-                          mainAxisSpacing: 10.0,
-                        ),
-                        itemBuilder: (BuildContext context, int index) {
-                          return CategoryCard(
-                            text: categories[index]['text']!,
-                            imageUrl: categories[index]['imageUrl']!,
-                          );
-                        },
-                      ),
+                      child: CategoriesGrid(),
                     ),
                   ],
                 ),
