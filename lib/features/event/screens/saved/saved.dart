e13018d2bb32b933956397dbba9b7adb1be54d95
SanudaKJ <sanudajayasinghe18@gmail.com>
2024-04-11 01:17:13 +0530

    feat: Saved page

--- lib/features/event/screens/saved/saved.dart
+++ lib/features/event/screens/saved/saved.dart
@@ -1,14 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:get/get_connect/http/src/utils/utils.dart';
+import 'package:uni_junction/common/widgets/appbar/appbar.dart';
+import 'package:uni_junction/common/widgets/custom_shapes/containers/primary_header_container.dart';
+import 'package:uni_junction/features/event/screens/create_event/widgets/create_event_appbar.dart';
+import 'package:uni_junction/features/event/screens/create_event/widgets/create_event_form.dart';
+import 'package:uni_junction/features/event/screens/home/widgets/this_month_event_all.dart';
+import 'package:uni_junction/features/event/screens/saved/widgets/saved_appbar.dart';
+import 'package:uni_junction/features/event/screens/saved/widgets/saved_event_.dart';
+import 'package:uni_junction/utils/constants/colors.dart';
+import 'package:uni_junction/utils/constants/text_strings.dart';
 
 class SavedScreen extends StatelessWidget {
   const SavedScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    // final Save = Get.put(SaveController());
     return const Scaffold(
-      body: Center(
-        child: Text('Saved Screen'),
+        body: SingleChildScrollView(
+      child: Column(
+        children: [
+          TPrimaryHeaderContainer(
+            height: 150,
+            backgroundColor: TColors.primary,
+            child: Column(
+              children: [
+                // Home AppBar
+                TSavedEventAppBar(),
+                SizedBox(height: 20),
+              ],
+            ),
+          ),
+          // TCreateEventForm(controller: Save,),
+          TThisMonthEventCards(),
+          SizedBox(height: 20),
+         TSavedEventCards(),
+        ],
       ),
-    );
+    ));
   }
 }
---
+++ lib/features/event/screens/saved/widgets/saved_event_.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:uni_junction/features/event/screens/home/widgets/this_month_event_cards.dart';
+import 'package:uni_junction/test/test_page.dart';
+import 'package:uni_junction/utils/constants/sizes.dart';
+
+class TSavedEventCards extends StatelessWidget {
+  const TSavedEventCards({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        EventThisMonth(
+          date: "12",
+          month: "Jan",
+          eventName: "Google I/O 24",
+          location: "Colombo, Sri Lanka",
+          imageUrl: "https://unsplash.it/645/411",
+          onTap: () => Get.to(() => TestScreen(
+              title: "Google I/O 24", description: "Colombo, Sri Lanka")),
+        ),
+        SizedBox(
+          height: TSizes.spaceBtwSections,
+        ),
+        EventThisMonth(
+          date: "03",
+          month: "Dec",
+          eventName: "Linux User Group",
+          location: "Colombo, Sri Lanka",
+          imageUrl: "https://unsplash.it/115/411",
+          onTap: () => Get.to(() => TestScreen(
+              title: "Linux User Group",
+              description: "this is linux user group")),
+        ),
+        SizedBox(
+          height: TSizes.spaceBtwSections,
+        ),
+        EventThisMonth(
+          date: "31",
+          month: "Apr",
+          eventName: "MS Champs",
+          location: "Microsoft, Sri Lanka",
+          imageUrl: "https://unsplash.it/915/411",
+        ),
+        SizedBox(
+          height: TSizes.spaceBtwSections,
+        ),
+        EventThisMonth(
+          date: "22",
+          month: "Jul",
+          eventName: "Arduino Day 2024",
+          location: "Colombo, Sri Lanka",
+          imageUrl: "https://unsplash.it/225/411",
+        ),
+        SizedBox(
+          height: TSizes.spaceBtwSections,
+        ),
+        EventThisMonth(
+          date: "24",
+          month: "Jul",
+          eventName: "GDG Devfest 2024",
+          location: "Colombo, Sri Lanka",
+          imageUrl: "https://unsplash.it/224/411",
+        ),
+        SizedBox(
+          height: TSizes.spaceBtwSections,
+        ),
+        EventThisMonth(
+          date: "09",
+          month: "Aug",
+          eventName: "KCD Sri Lanka 2024",
+          location: "Colombo, Sri Lanka",
+          imageUrl: "https://unsplash.it/687/411",
+        ),
+      ],
+    );
+  }
+}
