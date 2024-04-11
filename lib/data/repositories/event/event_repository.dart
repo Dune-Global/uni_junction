1eb0a77c29ec944f6788889eec64a2a73349d0d7
WasathTheekshana <wasath.vt@gmail.com>
2024-04-11 23:33:26 +0530

    feat: Register event

--- lib/data/repositories/event/event_repository.dart
+++ lib/data/repositories/event/event_repository.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:uni_junction/data/repositories/user/user_repository.dart';
 import 'package:uni_junction/features/event/models/event/event_model.dart';
 import 'package:uni_junction/utils/exceptions/firebase_exceptions.dart';
 import 'package:uni_junction/utils/exceptions/format_exceptions.dart';
@@ -28,9 +29,7 @@ class EventRepository extends GetxController {
   }
 
   Future<Map<String, int>> getEventCategoryCounts() async {
-    final querySnapshot = await _db
-        .collection("Events")
-        .get();
+    final querySnapshot = await _db.collection("Events").get();
     final docs = querySnapshot.docs;
 
     final Map<String, int> categoryCounts = {};
@@ -58,5 +57,70 @@ class EventRepository extends GetxController {
       events.add(EventModel.fromSnapshot(doc));
     }
     return events;
+  }
+
+  // push userId to the attendees arry
+  Future<void> pushUser(String eventId) async {
+    try {
+      final userReposotory = Get.find<UserRepository>();
+      final user = await userReposotory.fetchUserDetails();
+      return await _db.collection("Events").doc(eventId).update({
+        'attendees': FieldValue.arrayUnion([user.id])
+      });
+    } on FirebaseException catch (e) {
+      print(e.code);
+      throw TFirebaseException(e.code).message;
+    } on FormatException catch (_) {
+      throw const TFormatException();
+    } on PlatformException catch (e) {
+      throw TPlatformException(e.code).message;
+    } catch (e) {
+      print(e.toString());
+      throw e.toString();
+    }
+  }
+
+  // Remove userId from the attendees array
+  Future<void> removeUser(String eventId) async {
+    try {
+      final userReposotory = Get.find<UserRepository>();
+      final user = await userReposotory.fetchUserDetails();
+      return await _db.collection("Events").doc(eventId).update({
+        'attendees': FieldValue.arrayRemove([user.id])
+      });
+    } on FirebaseException catch (e) {
+      throw TFirebaseException(e.code).message;
+    } on FormatException catch (_) {
+      throw const TFormatException();
+    } on PlatformException catch (e) {
+      throw TPlatformException(e.code).message;
+    } catch (e) {
+      print(e.toString());
+      throw e.toString();
+    }
+  }
+
+  // Check if events has current user's id in attendees array
+  Future<bool> isUserAttending(String eventId) async {
+    try {
+      final userReposotory = Get.find<UserRepository>();
+      final user = await userReposotory.fetchUserDetails();
+      final documentSnapshot =
+          await _db.collection("Events").doc(user.id).get();
+      if (documentSnapshot.exists) {
+        final attendees = documentSnapshot.data()!['attendees'];
+        return attendees.contains(eventId);
+      }
+      return false;
+    } on FirebaseException catch (e) {
+      throw TFirebaseException(e.code).message;
+    } on FormatException catch (_) {
+      throw const TFormatException();
+    } on PlatformException catch (e) {
+      throw TPlatformException(e.code).message;
+    } catch (e) {
+      print(e.toString());
+      throw e.toString();
+    }
   }
 }
