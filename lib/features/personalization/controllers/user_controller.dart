04af334472908317f37d371f618ec1b5262931e0
Shenon69 <ask.tikki69@gmail.com>
2024-04-12 06:01:01 +0530

    refactor: User controller

--- lib/features/personalization/controllers/user_controller.dart
+++ lib/features/personalization/controllers/user_controller.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
-import 'package:get_storage/get_storage.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:uni_junction/data/repositories/user/user_repository.dart';
 import 'package:uni_junction/features/personalization/models/user_model.dart';
