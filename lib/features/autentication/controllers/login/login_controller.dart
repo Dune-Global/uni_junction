decaaa5c8c99670c5eb1e1c8cda3958c863367cf
Shenon69 <ask.tikki69@gmail.com>
2024-04-12 06:02:34 +0530

    refactor: Login imports

--- lib/features/autentication/controllers/login/login_controller.dart
+++ lib/features/autentication/controllers/login/login_controller.dart
@@ -3,7 +3,6 @@ import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:uni_junction/common/widgets/loaders/snackbar_popup.dart';
 import 'package:uni_junction/data/repositories/authentication/authentication_repository.dart';
-import 'package:uni_junction/features/event/screens/home/home.dart';
 import 'package:uni_junction/navigation_menu.dart';
 import 'package:uni_junction/utils/constants/image_strings.dart';
 import 'package:uni_junction/utils/popup/full_screen_loader.dart';
@@ -39,7 +38,7 @@ class LoginController extends GetxController {
 
       TFullScreenLoader.stopLoading();
 
-      Get.offAll(() => const NavigationMenu()); 
+      Get.offAll(() => const NavigationMenu());
     } catch (e) {
       TFullScreenLoader.stopLoading();
       TLoaders.errorSnackBar("Oh Snap", e.toString());
